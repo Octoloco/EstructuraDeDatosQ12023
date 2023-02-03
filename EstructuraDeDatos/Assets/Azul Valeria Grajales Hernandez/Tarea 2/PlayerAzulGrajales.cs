1ba9a6d1ccb4e440ccba7f83ba9943579425b26e
AzulGrajales <azul.grajales35@my.unitec.edu.mx>
2023-02-03 00:25:46 -0600

    Colliders y testeo
    sale mal

--- EstructuraDeDatos/Assets/Azul Valeria Grajales Hernandez/Tarea 2/PlayerAzulGrajales.cs
+++ EstructuraDeDatos/Assets/Azul Valeria Grajales Hernandez/Tarea 2/PlayerAzulGrajales.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAzulGrajales : MonoBehaviour
-
-
-
 {
+    [SerializeField] private float speed = 4;
 
-
-    [SerializeField] private float speed = 3;
-
-        int life = 3;
-        bool death = false;
-        float points = 0;
-        int bullets = 5;
-        int level = 0;
+    int life = 3;
+    bool death = false;
+    float points = 0;
+    int bullets = 5;
+    int level = 0;
 
 
     void Start()
     {
-        
-        
+
+
     }
 
     void Update()
@@ -32,28 +27,22 @@ public class PlayerAzulGrajales : MonoBehaviour
 
     private void Move()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
-        }
-
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.translate(Vector3.forward * Time.deltaTime * speed);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.translate(Vector3.Back * Time.deltaTime * speed);
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
     }
 }
-
-
-
-    
-
-
