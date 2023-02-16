caf642632b5aaf7937f33eae45d0eaba044bbbc3
FriMon24 <123040719+FriMon24@users.noreply.github.com>
2023-02-16 12:52:35 -0600

    Commit

---
+++ EstructuraDeDatos/Assets/Frida Montserrat Lagunas González/Homework1/Script/PlayerFridaLagunas.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFridaLagunas : MonoBehaviour
+{
+    int life;
+    bool death;
+    int shootcount;
+    bool kill;
+    int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 3;
+        death = true;
+        shootcount = 5;
+        kill = true;
+        speed = 3;
+    
+    }
+    void Jump()
+    {
+
+    }
+
+    void Move ()
+    {
+
+    }
+
+    void Shoot ()
+    {
+
+    }
+
+    void Attack ()
+    {
+
+    }
+
+    void ChangeWeapon()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ EstructuraDeDatos/Assets/Frida Montserrat Lagunas González/Homework2/MovePLayerFrida.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePLayerFrida : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+}
